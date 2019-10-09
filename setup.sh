0c52bf033d7d5e636dc8ef137983ba0eb9577bd3
Phil R <philr_brighton@yahoo.co.uk>
2019-10-10 00:49:30 +0100

    Add my setup script in case I need to rebuild (Cygwin) Add some Windows (Cygwin) files to .gitignore

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ aclocal
 autoheader
 automake --force-missing --add-missing
 autoconf
-./configure CFLAGS="-O2 -march=native -pipe -flto"
+./configure CPPFLAGS='-O2' CFLAGS='-O2' PKG_CONFIG_PATH='/usr/local/lib/pkgconfig'
